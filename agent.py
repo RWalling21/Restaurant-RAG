f91cfce0fd0dd3f82a95a47a87ee0b37113179b4
RWalling21 <rhw8246@rit.edu>
2024-06-08 17:42:13 -0400

    Refinement

--- agent.py
+++ agent.py
@@ -12,7 +12,7 @@ _ = load_dotenv()
 
 class AgentState(TypedDict):
     messages: Annotated[list[AnyMessage], operator.add]
-    json_output: Annotated[Dict[str, Any], operator.add]
+    json_output: str
 
 class Agent:
     def __init__(self, model, tools, system=""):
@@ -63,28 +63,14 @@ prompt = """You are an AI assistant tasked with scraping web data for database u
 Your specific duties involve collecting detailed information about restaurants, including their names, phone numbers, emails, dietary options, and customer reviews.
 
 Follow these guidelines: 
-- Utilize the search engine to identify relevant web pages. 
-- Employ the read-tool to extract necessary data from URLs obtained via the search engine. 
+- Utilize the search engine to identify relevant web pages and begin to gather information. 
+- Employ the read-tool to dig deaper and read links mentioned from the seach tool. 
 - Make multiple search and read-tool calls as needed, either concurrently or sequentially. 
 - Initiate searches only when the required information parameters are clearly defined. 
 - If preliminary information is needed for a follow-up query, obtain it before proceeding with further questions. 
 - Once data collection is complete, compile and return the information exclusively in JSON format. Interaction with the user is not required beyond providing the JSON output.
 - Collect enough information to make at least 5 complete JSON objects.
 - All data MUST BE REAL, only include information that has been directly sited from a review site / restaurant website. 
-
-Expected JSON output format: 
-```json
-{
-  "restaurants": [
-    {
-      "name": "Example Restaurant",
-      "phone_number": "000-000-0000",
-      "email": "info@example.com",
-      "dietary_offerings": ["vegan", "nut-free"],
-      "customer_reviews": ["Example user review"]
-    }
-  ]
-}
 """
 
 model = ChatOpenAI(model="gpt-4o")
--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from langchain_core.messages import HumanMessage
 from agent import agent
-
 from dotenv import load_dotenv
+import json
+
 _ = load_dotenv()
 
-messages = [HumanMessage(content="I need low fat foods in Washington D.C.")]
-result = agent.graph.invoke({"messages": messages})
-json_date = result['json_output']
-print(json_date)
+if __name__ == "__main__":
+    user_input = input("Please enter your query: ")
+    messages = [HumanMessage(content=user_input)]
+
+    result = agent.graph.invoke({"messages": messages})
+
+    json_data = json.loads(result['json_output'][-1])
+    json_string = json.dumps(json_data, indent=2)
+    print(json_string)
